79fd6c2fa45141e20c88fce14ac50b13c58480db
Ko Ko Myo Hein Htet <kokomyoheinhtet@gmail.com>
2023-09-25 18:56:24 +0700

    added static http

--- src/controller/video-controller.go
+++ src/controller/video-controller.go
@@ -6,11 +6,13 @@ import (
 	"github.com/kkmhh/practice-go-gin/src/service"
 	"github.com/kkmhh/practice-go-gin/src/validators"
 	"gopkg.in/go-playground/validator.v9"
+	"net/http"
 )
 
 type VideoController interface {
 	FindAll() []entity.Video
 	Save(ctx *gin.Context) error
+	ShowAll(ctx *gin.Context)
 }
 
 type controller struct {
@@ -43,4 +45,13 @@ func (c *controller) Save(ctx *gin.Context) error {
 	}
 	c.service.Save(video)
 	return nil
+}
+
+func (c *controller) ShowAll(ctx *gin.Context) {
+	videos := c.service.FindAll()
+	data := gin.H{
+		"title":  "Video Page",
+		"videos": videos,
+	}
+	ctx.HTML(http.StatusOK, "index.html", data)
 }
--- src/server.go
+++ src/server.go
@@ -27,6 +27,10 @@ func main() {
 
 	server := gin.New()
 
+	server.Static("/css", "./templates/css")
+
+	server.LoadHTMLGlob("templates/*.html")
+
 	server.Use(
 		gin.Recovery(),
 		middlewares.Logger(),
@@ -34,18 +38,26 @@ func main() {
 		//gindump.Dump(),
 	)
 
-	server.GET("/videos", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, videoController.FindAll())
-	})
+	apiRoutes := server.Group("/api")
+	{
+		apiRoutes.GET("/videos", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, videoController.FindAll())
+		})
 
-	server.POST("/videos", func(ctx *gin.Context) {
-		err := videoController.Save(ctx)
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		} else {
-			ctx.JSON(http.StatusOK, gin.H{"message": "Video Valid."})
-		}
-	})
+		apiRoutes.POST("/videos", func(ctx *gin.Context) {
+			err := videoController.Save(ctx)
+			if err != nil {
+				ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			} else {
+				ctx.JSON(http.StatusOK, gin.H{"message": "Video Valid."})
+			}
+		})
+	}
+
+	viewRoutes := server.Group("/view")
+	{
+		viewRoutes.GET("/videos", videoController.ShowAll)
+	}
 
 	server.Run(":8080")
 }
